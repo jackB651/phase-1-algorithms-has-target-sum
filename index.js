84f4eed06981368942a7242d6600c25ce33cba98
Jonathon <jwbialas@gmail.com>
2022-07-11 21:15:33 -0500

    commit tree b582d2bd0932651aed8f4a93454a7b3e1f806903

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+     // for the current number, identify a complementary number that adds to our target
+    // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+    const complement = target - array[i];
+    // iterate over the remaining numbers in the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any of the remaining numbers is the complement
+      // if so, return true
+      if (array[j] === complement) return true;
+    }
+  }
+  return false
 }
 
 /* 
@@ -8,10 +20,19 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  goes through the array and adds it with each of the following elements. If the result equals target,
+  returns true if not returns false. 
 */
 
 // You can run `node index.js` to view these console logs
